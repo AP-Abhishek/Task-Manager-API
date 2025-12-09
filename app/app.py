c9ff287efcc715c350606203f77f6c770ebce4c9
AP-Abhishek <abhishekap12005@gmail.com>
2025-12-09 21:05:06 +0530

    feat: Implemented JWT based access tokens.

--- app/app.py
+++ app/app.py
@@ -1,20 +1,26 @@
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, Header
 from sqlalchemy.orm import Session
+from datetime import timedelta
 
 from db import Base, engine, get_db
 from models.Task import Task
 from models.User import User
 from schema.TaskSchema import TaskSchema
 from schema.UserSchema import UserSchema
+from utils.jwt import create_access_token, decode_access_token
 
 app = FastAPI(title="Task Manager API")
 Base.metadata.create_all(bind=engine)
 
-def get_current_user(token: str, db: Session):
-    if not token.startswith("token_"):
-        return None
-    user_id = token.replace("token_", "")
-    user = db.query(User).filter(User.id == user_id).first()
+def get_current_user(token: str = Header(None), db: Session = Depends(get_db)) -> User:
+    payload = decode_access_token(token)
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid token.")
+
+    username = payload.get("sub")
+    user = db.query(User).filter(User.username == username).first()
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found.")
     return user
 
 @app.post("/login")
@@ -22,9 +28,11 @@ def login(user: UserSchema, db: Session = Depends(get_db)):
     db_user = db.query(User).filter(User.username == user.username).first()
     if not db_user or db_user.password != user.password:
         raise HTTPException(status_code=400, detail="Invalid username or password.")
+    
+    access_token_expires = timedelta(minutes=30)
+    access_token = create_access_token(data={"sub": db_user.username}, expires_delta=access_token_expires)
 
-    session_token = f"token_{db_user.id}"
-    return {"message": "Login Successfull", "session": session_token}
+    return {"message": "Login Successfull", "access_token": access_token}
 
 @app.post("/signin")
 def sign_in(user: UserSchema, db: Session = Depends(get_db)):
@@ -38,29 +46,18 @@ def sign_in(user: UserSchema, db: Session = Depends(get_db)):
     return {"message": "New User created successfully...!"}
 
 @app.get("/tasks")
-def get_tasks(token: str, db: Session = Depends(get_db)):
-    user = get_current_user(token, db)
-    if not user:
-        raise HTTPException(status_code=401, detail="Invalid session.")
+def get_tasks(user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     return db.query(Task).filter(Task.user_id == user.id).all()
 
 @app.get("/tasks/{task_id}")
-def get_single_task(task_id: int, token: str, db: Session = Depends(get_db)):
-    user = get_current_user(token, db)
-    if not user:
-        raise HTTPException(status_code=401, detail="Invalid session.")
-    
+def get_single_task(task_id: int, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     task = db.query(Task).filter(Task.user_id == user.id, Task.id == task_id).first()
     if not task:
         raise HTTPException(status_code=404, detail="Task not found.")
     return {"task": task}
 
 @app.post("/tasks")
-def add_task(task: TaskSchema, token: str, db: Session = Depends(get_db)):
-    user = get_current_user(token, db)
-    if not user:
-        raise HTTPException(status_code=401, detail="Invalid session.")
-    
+def add_task(task: TaskSchema, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     new_task = Task(
         title = task.title,
         description = task.description,
@@ -73,11 +70,7 @@ def add_task(task: TaskSchema, token: str, db: Session = Depends(get_db)):
     return {"message": "Task added successfully...!", "id": new_task.id}
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int, token: str, updated: TaskSchema, db: Session = Depends(get_db)):
-    user = get_current_user(token, db)
-    if not user:
-        raise HTTPException(status_code=401, detail="Invalid session.")
-
+def update_task(task_id: int, updated: TaskSchema, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     task = db.query(Task).filter(Task.user_id == user.id, Task.id == task_id).first()
     if not task:
         raise HTTPException(status_code=404, detail="Task not found.")
@@ -90,11 +83,7 @@ def update_task(task_id: int, token: str, updated: TaskSchema, db: Session = Dep
     return {"message": "Task updated successfully...!", "task": task}
 
 @app.delete("/tasks/{task_id}")
-def delete_task(task_id: int, token: str, db: Session = Depends(get_db)):
-    user = get_current_user(token, db)
-    if not user:
-        raise HTTPException(status_code=401, detail="Invalid session.")
-    
+def delete_task(task_id: int, user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     task = db.query(Task).filter(Task.user_id == user.id, Task.id == task_id).first()
     if not task:
         raise HTTPException(status_code=404, detail="Task not found.")
@@ -103,11 +92,7 @@ def delete_task(task_id: int, token: str, db: Session = Depends(get_db)):
     return {"message": "Task deleted successfully...!"}
 
 @app.delete("/tasks")
-def delete_all_tasks(token: str, db: Session = Depends(get_db)):
-    user = get_current_user(token, db)
-    if not user:
-        raise HTTPException(status_code=401, detail="Invalid session.")
-    
+def delete_all_tasks(user: User = Depends(get_current_user), db: Session = Depends(get_db)):
     db.query(Task).filter(Task.user_id == user.id).delete()
     db.commit()
     return {"message": "Deleted all tasks."}
---
+++ app/utils/jwt.py
@@ -0,0 +1,28 @@
+import jwt, os
+from datetime import datetime, timedelta
+from typing import Optional
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+
+def create_access_token(data: dict, expires_delta: Optional[timedelta] = None) -> str:
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+def decode_access_token(token: str) -> dict:
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except jwt.PyJWTError:
+        return None
